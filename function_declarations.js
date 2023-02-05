e9b662d9464c1e39de7fa97a8a7bebc9d817433a
nkchambers <nkchames@gmail.com>
2023-02-05 15:29:44 -0800

    adding function_declarations for 2-5-23

---
+++ function_declarations.js
@@ -0,0 +1,27 @@
+//CODECADEMY JAVASCRIPT PRACTICE - function_declarations assignment
+
+// Task 1 - Let’s create a function that prints a reminder to the console. 
+// Using a function declaration, create a function called getReminder().
+
+// Task 2 - In the function body of getReminder(), log the following reminder to the console: 'Water the plants.'
+
+function getReminder() {
+    console.log('Water the plants.');
+}
+
+// Calling the function - functionName();
+getReminder();
+
+
+// Task 3 - Let’s create another function that prints a useful Spanish travel phrase to the console.
+// Using a function declaration, create a function called greetInSpanish().
+
+//Tast 4 - Add code to the function body of greetInSpanish():
+// In the function body console.log() the following Spanish phrase to the console: 'Buenas tardes.'
+
+function greetInSpanish() {
+    console.log('Buenas tardes.');
+}
+
+// Calling the function - functionName();
+greetInSpanish();
